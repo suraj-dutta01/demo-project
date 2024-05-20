80ea36513a3d3458f3061131063e67f1555e9490
suraj-dutta01 <surajdutta.skd@gmail.com>
2024-05-20 14:46:53 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Demo{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=sc.next();
+        System.out.println(name);
+    }
+}
